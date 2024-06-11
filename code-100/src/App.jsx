513b1e919242f6c520f801f503e1ad780b0f622a
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-11 03:06:03 +0100

    double noise flow field

--- code-100/src/App.jsx
+++ code-100/src/App.jsx
@@ -15,6 +15,11 @@ function App() {
       <div className="container">
       <p>2. Mood Calendar</p> <div className="rule"></div> <p>Day 2</p>
       </div>
+
+      <div className="container">
+      <p>3. Double Noise flow field</p> <div className="rule"></div> <p>Day 3</p>
+      </div>
+      
       
 
       <RandomMeal />
