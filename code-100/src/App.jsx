ebf167e6f3f81d8fcaa93ca339f5cdd52eab371a
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-11 04:44:33 +0100

    main page init

--- code-100/src/App.jsx
+++ code-100/src/App.jsx
@@ -1,4 +1,5 @@
  import RandomMeal from "./RandomMeal"
+ import Mainpage from "./components/Mainpage"
 
 function App() {
 
@@ -6,22 +7,7 @@ function App() {
   return (
     <>
       <h1>100 days of Code</h1>
-
-
-      <div className="container">
-      <p>1. Random Meal Generator</p> <div className="rule"></div> <p>Day 1</p>
-      </div>
-
-      <div className="container">
-      <p>2. Mood Calendar</p> <div className="rule"></div> <p>Day 2</p>
-      </div>
-
-      <div className="container">
-      <p>3. Double Noise flow field</p> <div className="rule"></div> <p>Day 3</p>
-      </div>
-      
-      
-
+      <Mainpage/>
       <RandomMeal />
     </>
   )
---
+++ code-100/src/components/Mainpage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Mainpage = () => {
+  return (
+    <div>
+      <div className="container">
+      <p>1. Random Meal Generator</p> <div className="rule"></div> <p>Day 1</p>
+      </div>
+
+      <div className="container">
+      <p>2. Mood Calendar</p> <div className="rule"></div> <p>Day 2</p>
+      </div>
+
+      <div className="container">
+      <p>3. Double Noise flow field</p> <div className="rule"></div> <p>Day 3</p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Mainpage
