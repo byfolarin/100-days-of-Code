f40d470020d5828f50cfaf9acd2285678dcf6027
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-11 05:00:01 +0100

    mapping list

--- code-100/src/components/Mainpage.jsx
+++ code-100/src/components/Mainpage.jsx
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Mainpage = () => {
+
+    const [conditions, setConditions] = useState('');
+
+    const details = [
+            {
+                id: 1,
+                task: "Random meal Generator",
+                day: "Day 1" 
+            },
+
+            { id: 2,
+                task: "Mood Calendar",
+                day: "Day 2",
+            },
+
+    ]
+
+
+    const deets = details.map(item =>{
+         <li key={item.id}>{item.task}</li>
+    })
+
+
   return (
     <div>
       <div className="container">
-      <p>1. Random Meal Generator</p> <div className="rule"></div> <p>Day 1</p>
+            <p>{deets}</p> 
+            <div className="rule"></div>
+            <p>Day 1</p>
       </div>
 
       <div className="container">
-      <p>2. Mood Calendar</p> <div className="rule"></div> <p>Day 2</p>
+            <p>2. Mood Calendar</p>
+            <div className="rule"></div>
+            <p>Day 2</p>
       </div>
 
       <div className="container">
-      <p>3. Double Noise flow field</p> <div className="rule"></div> <p>Day 3</p>
+            <p>3. Double Noise flow field</p>
+            <div className="rule"></div> 
+            <p>Day 3</p>
       </div>
       
     </div>
