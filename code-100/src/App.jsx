fb036888f11290723464d882f36b9918dc99fa5f
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-18 17:51:42 +0100

    stressed code

--- code-100/src/App.jsx
+++ code-100/src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; // Import React Router components
 import Mainpage from './Mainpage';
 
 // Main App component
 function App() {
   return (
-   
-
+    <BrowserRouter>
+    
       <>
         <h1>100 Days of Code</h1>
         <Mainpage />
       </>
-      
-   
+
+      <Routes>
+        <Route path="/Mainpage" element={<Mainpage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- code-100/src/Mainpage.jsx
+++ code-100/src/Mainpage.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; // Import React Router components
+
 
 const Mainpage = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+
+            <ul className='container'>
+            <li className='display-title'>
+            <Link to="/Todolist"> 
+                  <p>Todolist</p>
+                  <div className='rule'></div>
+                  <p>Day 1</p>
+            </Link>
+            </li>
+            </ul>
+
+    </>
   )
 }
 
