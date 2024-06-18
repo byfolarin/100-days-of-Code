67240caded59b29f3e8435c49b4baf52836d1d7a
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-18 16:49:34 +0100

    Routes

--- code-100/src/App.jsx
+++ code-100/src/App.jsx
@@ -2,7 +2,6 @@ import Mainpage from "./Mainpage"
 
 function App() {
 
-
   return (
     <>
       <h1>100 days of Code</h1>
--- code-100/src/Mainpage.jsx
+++ code-100/src/Mainpage.jsx
@@ -1,45 +1,48 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 import Todolist from './components/Todolist'
+import {Routes,Route, Link} from 'react-router-dom'
 
 const Mainpage = () => {
 
-    const details = [
-            {
-                id: 1,
-                task: "Random meal Generator",
-                day: "Day 1" 
-            },
+//     const details = [
+//             {
+//                 id: 1,
+//                 task: "Random meal Generator",
+//                 day: "Day 1" 
+//             },
 
-            { id: 2,
-                task: "Mood Calendar",
-                day: "Day 2",
-            },
+//             { id: 2,
+//                 task: "Mood Calendar",
+//                 day: "Day 2",
+//             },
 
-    ]
+//     ]
 
 
-    const deets = details.slice(0, 1).map(item =>(
-         <li key={item.id}> {item.id}  {item.task} </li>
-    ))
+//     const deets = details.slice(0, 1).map(item =>(
+//          <li key={item.id}> {item.id}  {item.task} </li>
+//     ))
 
-    const sets = details.slice(1, 2).map(item =>(
-        <li key={item.id}> {item.id}  {item.task} </li>
-   ))
+//     const sets = details.slice(1, 2).map(item =>(
+//         <li key={item.id}> {item.id}  {item.task} </li>
+//    ))
 
-        const body = 2;
+//         const body = 2;
 
 
 
   return (
+
+
     <div>
-      <div className="container">
-            <p>TodoList</p> 
-            <div className="rule"></div>
-            <p>Day 1</p>
-      </div>
-      <Todolist />
 
+          <ul className="container">
+          <Link to="/Todolist" className='navv'><p>TodoList</p> <div className="rule"></div><p>Day 1</p></Link>     
+          </ul>
 
+      <Routes>
+            <Route path='/Todolist' element={<Todolist />}></Route>  
+      </Routes>
     </div>
   )
 }
