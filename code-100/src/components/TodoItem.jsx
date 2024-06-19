41fa36617287ddad809e5583bdfb54c2b0c40c06
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-19 08:35:20 +0100

    completed todo list for day 1

--- code-100/src/components/TodoItem.jsx
+++ code-100/src/components/TodoItem.jsx
@@ -1,21 +1,11 @@
 import React from 'react'
 
-const TodoItem = ({input,arrList,setArrList}) => {
-
-    const getArr = () =>{
-        setArrList(i =>[...i, input])
-    }
-
-    // const itemList = {
-    //     id: Math.random() * 100,
-
-    // }
+const TodoItem = () => {
 
 
   return (
-    <div>
-      <li>{arrList}</li>
-    </div>
+    <>
+    </>
   )
 }
 
--- code-100/src/components/Todolist.jsx
+++ code-100/src/components/Todolist.jsx
@@ -1,45 +1,78 @@
-import React, { useEffect, useState } from 'react'
-import Modal from './unused comp/Modal'
+import React, { useEffect, useState } from 'react';
+import Modal from './unused comp/Modal';
 
-const Todolist = () => {
+const TodoList = () => {
+  const [input, setInput] = useState("");
+  const [arrList, setArrList] = useState([]);
+  const [prompts, setPrompts] = useState("");
 
-    const [input, setInput] = useState("");
-    const [arrList, setArrList] = useState([]);
 
-function getInput(e){
-    setInput(e.target.value);
-}
+    function sortClick (){
+        const inputName = prompt("what's your name");
+        setPrompts(inputName); 
+    }
 
+
+
+
+  const month = new Date().getMonth();
+  const day = new Date().getDay().toString();
+  const newDate = month === 5 ? "June" : month; // No need for toString
 
   
+  function handleClick(){
+    setArrList((prevArr) => {
+      if (input.trim()) 
+        {return [...prevArr, {id: Math.round(Math.random() * 100) + 1,task: input,date: `${newDate }  ${day}`,}];}
+    }); setInput(" ")  }
+
+    
+    const keys = (event) => {
+        if (event.keyCode === 13) 
+          handleClick();
+        }
+ 
+
+  const getInput = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
-    <div className='todo-gap'>
+    <div className="todo-gap">
+      <Modal />
+    <button onClick={sortClick}>Click Here first</button>
+      <h1 className="todo-name">Hello: {prompts}</h1>
 
-        <Modal></Modal>
+      <div className="input-button">
+        <input
+          type="text"
+          name=""
+          value={input}
+          onKeyDown={keys}
+          onChange={getInput} // Dependency on 'input' added here
+          id="search-bar"
+          className="input-search"
+          placeholder="+ Insert a link, color, or just plain text..."
+        />
+        <button onClick={handleClick}>+</button>
+      </div>
 
-        <h1 className='todo-name'>Hello Folarin</h1>
+      <div className="div-title">
+        <p>Title</p>
+        <p>Date Created</p>
+      </div>
 
-            <div className="input-button">
-            <input type='text' name="" value={input} onChange={getInput} id="search-bar" 
-                className='input-search'
-                placeholder='+ Insert a link, color, or just plain text...'/>
-                <button onClick={getInput}>+</button>
-                </div>
+      <div className="rule"></div>
 
-        <div className="div-title">
-            <p>Title</p>
-            <p>Date Created</p>
-        </div>
-
-        <div className="rule"></div>
-
-
-        <div className="div-list-item">
-            <li>{input}</li>
-        </div>
-
+      <div className="div-list-item">
+        {arrList.map((item) => (
+          <li className='list-keys' key={item.id}>
+            <span>{item.task} </span> <span>{item.date}</span>  
+          </li>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Todolist
+export default TodoList;
