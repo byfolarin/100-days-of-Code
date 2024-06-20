71b4853f064b8030298011a8c798c66067175bd8
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-20 14:43:28 +0100

    img gallery update

--- code-100/src/components/ImageGallery.jsx
+++ code-100/src/components/ImageGallery.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import img1 from '../assets/images/img1.jpeg'
+import img2 from '../assets/images/img2.jpeg'
+import img3 from '../assets/images/img3.jpeg'
+import img4 from '../assets/images/img4.jpeg'
+import img5 from '../assets/images/img5.jpeg'
 
 const ImageGallery = () => {
   return (
@@ -8,13 +12,40 @@ const ImageGallery = () => {
         <img src={img1} alt="" />
       </div>
       <div className="grid-img">
-        <div className="img-1 slide">
-             <img src={img1} alt="" />
+                <div className="img-1 slide">
+                    <img src={img1} alt="" />
+                </div>
+        <div className="img-2 slide">
+                    <div className="img-1 slide">
+                        <img src={img2} alt="" />
+                    </div>
         </div>
-        <div className="img-2 slide"></div>
-        <div className="img-3 slide"></div>
-        <div className="img-3 slide"></div>
-        <div className="img-4 slide"></div>
+
+
+        <div className="img-3 slide">
+                    <div className="img-1 slide">
+                          <img src={img3} alt="" />
+                     </div>
+        </div>
+
+
+
+        <div className="img-3 slide">
+
+                    <div className="img-1 slide">
+                        <img src={img4} alt="" />
+                    </div>
+
+        </div>
+
+
+        <div className="img-4 slide">
+
+                <div className="img-1 slide">
+                    <img src={img5} alt="" />
+                </div>
+
+        </div>
         
       </div>
     </div>
