6031dcd381cbb72829bbc6d62bafa04a6c8c9907
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-20 11:21:44 +0100

    img container

--- code-100/src/components/ImageGallery.jsx
+++ code-100/src/components/ImageGallery.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 const ImageGallery = () => {
   return (
     <div>
-      <div className="img-container">
+      <div className="img-container"></div>
+      <div className="grid-img">
+        <div className="img-1 slide"></div>
+        <div className="img-2 slide"></div>
+        <div className="img-3 slide"></div>
+        <div className="img-3 slide"></div>
+        <div className="img-4 slide"></div>
         
       </div>
     </div>
