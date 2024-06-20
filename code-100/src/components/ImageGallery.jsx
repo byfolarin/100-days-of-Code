3675afa20e0b2db50859e9f53b5beb695c00370c
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-20 11:34:17 +0100

    uploaded sect

--- code-100/src/components/ImageGallery.jsx
+++ code-100/src/components/ImageGallery.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import img1 from '../assets/images/img1.jpeg'
 
 const ImageGallery = () => {
   return (
     <div>
-      <div className="img-container"></div>
+      <div className="img-container">
+        <img src={img1} alt="" />
+      </div>
       <div className="grid-img">
         <div className="img-1 slide"></div>
         <div className="img-2 slide"></div>
