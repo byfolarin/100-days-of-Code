cd7a4da27c90a85269ebe751716ae2b768685ada
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-24 05:09:07 +0100

    boolean

--- code-100/src/components/FAQ.jsx
+++ code-100/src/components/FAQ.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FAQ = () => {
+
+    const [paragraph, setParagraph] = useState(true);
+
+
+    function handleClick (){
+        setParagraph(!paragraph);
+    }
+ 
+
+
   return (
     <>
       <h1>FAQ</h1>
@@ -9,28 +19,33 @@ const FAQ = () => {
         <ul>
             <li>
                 <h3>The Simulation Argument</h3>
-                <p>If advanced civilizations can create near-indistinguishable simulations, and the number of simulations vastly outweighs the number of "base" realities, wouldn't it be statistically likely that we ourselves are living in a simulation?  How can we tell if our reality is real or a simulated one?z</p>
+                <p className={paragraph ? 'hovered' : 'not-hovered'} onClick={handleClick}>If advanced civilizations can create near-indistinguishable simulations, and the number of simulations vastly outweighs the number of "base" realities, wouldn't it be statistically likely that we ourselves are living in a simulation?  How can we tell if our reality is real or a simulated one?z</p>
             </li>
+
+            <hr />
 
             <li>
                 <h3>The Fermi Paradox</h3>
-                <p> Given the vastness of the universe and the probability of life arising on other planets, where is everyone?  The Fermi Paradox highlights the apparent contradiction between the high estimates of the probability of extraterrestrial life and the lack of evidence for it.  Is it possible we're simply too early or too late in the cosmic timeline to encounter other advanced</p>
+                <p className='onFAQ' onClick={handleClick}> Given the vastness of the universe and the probability of life arising on other planets, where is everyone?  The Fermi Paradox highlights the apparent contradiction between the high estimates of the probability of extraterrestrial life and the lack of evidence for it.  Is it possible we're simply too early or too late in the cosmic timeline to encounter other advanced</p>
             </li>
+
+            <hr />
 
             <li>
                 <h3>The Arrow of Time</h3>
-                <p>Why does time seem to flow in one direction (forward) and not the other (backward)?  Thermodynamic laws suggest an increase in entropy (disorder) over time.  Is the arrow of time an intrinsic property of the universe or an emergent phenomenon?</p>
+                <p className='onFAQ' onClick={handleClick}>Why does time seem to flow in one direction (forward) and not the other (backward)?  Thermodynamic laws suggest an increase in entropy (disorder) over time.  Is the arrow of time an intrinsic property of the universe or an emergent phenomenon?</p>
             </li>
+            <hr />
 
             <li>
                 <h3>The Quantum Observer Effect</h3>
-                <p> Does the act of observation collapse the wave function of a quantum particle, forcing it into a definite state?  Or does observation simply reveal the pre-existing state?  This thought experiment delves into the nature of reality and the observer's role in shaping it.</p>
+                <p className='onFAQ' onClick={handleClick}> Does the act of observation collapse the wave function of a quantum particle, forcing it into a definite state?  Or does observation simply reveal the pre-existing state?  This thought experiment delves into the nature of reality and the observer's role in shaping it.</p>
             </li>
 
-            
+            <hr />
             <li>
                 <h3>The Panpsychism Hypothesis</h3>
-                <p> Is consciousness fundamental to reality, or is it an emergent property of complex systems like brains?  Panpsychism suggests some level of consciousness might be present in all things, from elementary particles to planets.  Exploring this idea pushes the boundaries of our understanding of consciousness and its place in the universe.</p>
+                <p className='onFAQ' onClick={handleClick}> Is consciousness fundamental to reality, or is it an emergent property of complex systems like brains?  Panpsychism suggests some level of consciousness might be present in all things, from elementary particles to planets.  Exploring this idea pushes the boundaries of our understanding of consciousness and its place in the universe.</p>
             </li>
         </ul>
       </div>
