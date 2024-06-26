b82779944abc48018b5d57cccd1c59902357064a
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-26 20:45:07 +0100

    push commit

--- code-100/src/components/HeroSec.jsx
+++ code-100/src/components/HeroSec.jsx
@@ -2,16 +2,23 @@ import React from 'react'
 import vide1 from '../assets/images/video-2@c.mp4'
 
 const HeroSec = () => {
-  return (
-    <div>
-      <div className="hero-section-vid">
-        <div className="media">
-             <video src={vide1} width="540" height="320" controls muted poster="images/video-poster.jpg">Your browser doesn't support HTML video</video>
-      </div>
-        <h1>Crafting Purpose with Design</h1>
-      </div>
-    </div>
-  )
-}
+    return (
 
-export default HeroSec
+        <div className="hero-section-vid">
+
+                <div className="media">
+                    <video src={vide1} width={'520px'}controls muted>
+                    Your browser doesn't support HTML video
+                    </video>
+                </div>
+
+
+                <div className="div-h1">
+                    <h1>Crafting Purpose with Design</h1>
+                </div>
+
+        </div>
+      );
+    };
+    
+    export default HeroSec;
