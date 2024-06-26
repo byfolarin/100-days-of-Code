d24bde10578a049addcc6b1ee0276a6b09d8acbd
Folarin Folarin <thefolafolarin@gmail.com>
2024-06-26 12:57:30 +0100

    video-bg

--- code-100/src/components/HeroSec.jsx
+++ code-100/src/components/HeroSec.jsx
@@ -5,7 +5,9 @@ const HeroSec = () => {
   return (
     <div>
       <div className="hero-section-vid">
-      <video src={vide1} width="540" height="320" controls muted poster="images/video-poster.jpg">Your browser doesn't support HTML video</video>
+        <div className="media">
+             <video src={vide1} width="540" height="320" controls muted poster="images/video-poster.jpg">Your browser doesn't support HTML video</video>
+      </div>
         <h1>Crafting Purpose with Design</h1>
       </div>
     </div>
